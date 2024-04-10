9d114ede3cb4981f363f1a01259cd262e885e154
Leonor Pinto <160856234+LeonorTPinto@users.noreply.github.com>
2024-04-10 10:02:04 +0100

    Update Tablela-Biblioteca.sql

--- Aulas/Tablela-Biblioteca.sql
+++ Aulas/Tablela-Biblioteca.sql
@@ -10,12 +10,12 @@ USE Biblioteca
 GO
 -- criação tabela
 CREATE TABLE Aluno(
-	Numero_mecanografico Integer		NOT NULL,
-	Nome				 VARCHAR(50)	NOT NULL,
-	End_morada			 VARCHAR(50)	NOT NULL,
-	End_codigoPostal	 CHAR(8)		NOT NULL,
-	End_Localidade		 VARCHAR (50)	NOT NULL,
-	Garantia			 MONEY			NOT NULL DEFAULT 0,
+	Numero_mecanografico       Integer       NOT NULL,
+	Nome		           VARCHAR(50)	 NOT NULL,
+	End_morada	           VARCHAR(50)	 NOT NULL,
+	End_codigoPostal	   CHAR(8)	 NOT NULL,
+	End_Localidade		   VARCHAR (50)	 NOT NULL,
+	Garantia		   MONEY	 NOT NULL DEFAULT 0,
 	CHECK (Numero_mecanografico>0),
 	CHECK (End_codigoPostal LIKE '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
 	CHECK (Garantia >=0),
@@ -23,12 +23,12 @@ CREATE TABLE Aluno(
 	)
 
 CREATE TABLE Livro( 
-	Numero_Livro INTEGER NOT NULL IDENTITY(1,1),
-	Titulo VARCHAR(50)NOT NULL,
-	Autor  VARCHAR(50) NOT NULL,
-	Editor  VARCHAR(50)NOT NULL,
+	Numero_Livro  INTEGER       NOT NULL IDENTITY(1,1),
+	Titulo        VARCHAR(50)   NOT NULL,
+	Autor         VARCHAR(50)   NOT NULL,
+	Editor        VARCHAR(50)   NOT NULL,
 	Data_compra   SMALLDATETIME NOT NULL DEFAULT GETDATE(),
-	Estado  BIT NOT NULL DEFAULT 0,
+	Estado        BIT           NOT NULL DEFAULT 0,
 	---- Estado pode ser: requesitado('1') ou não requesitado('0')
 	PRIMARY KEY(Numero_Livro)
 	)
